4ac06ce2dd3f18d08d8ccb6f2d797ddec4bc0785
Mason Brill <masonjamesbrill@yahoo.com>
2024-05-26 19:21:02 -0400

    added more buildings and much better grass tiles for terrain along with more fences

--- Assets/scripts/cameramovement.cs
+++ Assets/scripts/cameramovement.cs
@@ -4,32 +4,14 @@ using UnityEngine;
 
 public class cameramovement : MonoBehaviour
 {
-    //variables
-    // private float horizontal = 0;
-    // private float vertical = 0;
-    // private float speed = 3f;
-
     [SerializeField] private Rigidbody2D player;
     [SerializeField] private Rigidbody2D mainCamera;
 
 
-    // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
-
     // Update is called once per frame
     void Update()
     {
-        // horizontal = Input.GetAxisRaw("Horizontal");
-        // vertical = Input.GetAxisRaw("Vertical");
         mainCamera.position = new Vector2(player.position.x, player.position.y);
         
     }
-
-    // private void FixedUpdate()
-    // {
-    //     rb.velocity = new Vector2(horizontal * speed, vertical * speed);
-    // }
 }
